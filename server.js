417f9515e492ff89b57c9dbf47654b529af87c6e
Peter Soboyejo <thepcmrtim@gmail.com>
2016-08-13 08:01:33 -0700

    launch for heroku

--- server.js
+++ server.js
@@ -778,18 +778,36 @@ router.get('/user/current', isAuthenticated, function(req, res) {
 
 router.get('/clinic/locations/search', function(req, res) {
 
+  var distance = function (p1, p2) {
+      var latR1 = toRadians(p1.lat);
+      var latR2 = toRadians(p2.lat);
+      var delta1 = toRadians(p2.lat - p1.lat);
+      var delta2 = toRadians(p2.lng - p1.lng);
+      var a = Math.sin(delta1/2) * Math.sin(delta1/2) +
+          Math.cos(latR1) * Math.cos(latR2) *
+          Math.sin(delta2/2) * Math.sin(delta2/2);
+      return (earthRadius * (2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))));
+  };
+
+  var earthRadius = 6378.137;
+
+  var toRadians = function (degrees) {
+      return degrees * Math.PI / 180;
+  };
+
   var zipToLongLang = cities.zip_lookup(req.query.zipcode);
 
   console.log(zipToLongLang);
 
   var limit = req.query.limit || 10;
 
-  var maxDistance = req.query.distance || 80; // 50 miles
-  maxDistance /= 6371;
+  var maxDistance = req.query.distance || 24.1402; // 15 miles in kms
+  maxDistance /= earthRadius;
   var coords = [];
 
   coords[0] = req.query.longitude || zipToLongLang.longitude;
   coords[1] = req.query.latitude || zipToLongLang.latitude;
+
 
   // find a location
   Clinic.find({
@@ -802,7 +820,13 @@ router.get('/clinic/locations/search', function(req, res) {
     if (err) {
       return res.json(500, err);
     }
-    res.json(200, locations);
+    var points = locations.map(function (location) {
+        location = location.toObject();
+        location.p = { lat: location.loc[1], lng: location.loc[0] };
+        location.distance = distance({ lat: coords[1], lng: coords[0] }, location.p);
+        return location;
+    });
+    res.json(200, points);
   });
 
 });
